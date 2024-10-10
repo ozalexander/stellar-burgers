4d0877e148d517ff6843b4ae3079ebf1bd0a922a
ozalexander <156056519+ozalexander@users.noreply.github.com>
2024-10-10 21:43:08 +0300

    Refactored

--- src/components/ingredients-category/ingredients-category.tsx
+++ src/components/ingredients-category/ingredients-category.tsx
@@ -2,17 +2,14 @@ import { forwardRef, useMemo } from 'react';
 import { TIngredientsCategoryProps } from './type';
 import { TIngredient } from '@utils-types';
 import { IngredientsCategoryUI } from '../ui/ingredients-category';
+import { useSelector } from '../../services/store';
+import { selectConstructorItems } from '../../slices/storeSlice';
 
 export const IngredientsCategory = forwardRef<
   HTMLUListElement,
   TIngredientsCategoryProps
 >(({ title, titleRef, ingredients }, ref) => {
-  const burgerConstructor = {
-    bun: {
-      _id: ''
-    },
-    ingredients: []
-  };
+  const burgerConstructor = useSelector(selectConstructorItems);
 
   const ingredientsCounters = useMemo(() => {
     const { bun, ingredients } = burgerConstructor;
@@ -21,7 +18,7 @@ export const IngredientsCategory = forwardRef<
       if (!counters[ingredient._id]) counters[ingredient._id] = 0;
       counters[ingredient._id]++;
     });
-    if (bun) counters[bun._id] = 2;
+    if (bun) counters[bun._id!] = 2;
     return counters;
   }, [burgerConstructor]);
 
