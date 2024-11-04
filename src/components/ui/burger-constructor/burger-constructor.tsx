518177e8f4118fda2bf11ea07f73d2dfb82ed6a8
ozalexander <156056519+ozalexander@users.noreply.github.com>
2024-11-04 23:20:19 +0300

    Cypress

--- src/components/ui/burger-constructor/burger-constructor.tsx
+++ src/components/ui/burger-constructor/burger-constructor.tsx
@@ -18,7 +18,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
   onOrderClick,
   closeOrderModal
 }) => (
-  <section className={styles.burger_constructor}>
+  <section className={styles.burger_constructor} data-cy='constructor'>
     {constructorItems.bun.name ? (
       <div className={`${styles.element} mb-4 mr-4`}>
         <ConstructorElement
@@ -84,6 +84,9 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
         size='large'
         children='Оформить заказ'
         onClick={onOrderClick}
+        disabled={
+          !constructorItems.bun.name || !constructorItems.ingredients.length
+        }
       />
     </div>
 
